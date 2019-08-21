b80daf24615ae9a6fb07a6b34c6986bc85542260
Nayomi Chibana <nchibana@gmail.com>
2019-08-21 03:20:23 -0400

    assignment completed

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,16 +1,42 @@
-"""Main application and routing logic for Twitoff"""
+"""Main application, configuration and routing logic for Twitoff"""
 
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, Tweet, User
+from .twitter import add_or_update_user
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = config('ENV')
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff!'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset', users=[])
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>')
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                db_user = add_or_update_user(name)
+                message = f"User {db_user.name} successfully added!"
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding or fetching {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets, 
+                                message=message)
 
     return app
---
+++ twitoff/twitter.py
@@ -0,0 +1,40 @@
+"""Retrieve tweets, embeddings and persist in the database"""
+import basilica
+import tweepy
+from decouple import config
+from .models import DB, Tweet, User
+
+TWITTER_AUTH = tweepy.OAuthHandler(config('TWITTER_CONSUMER_KEY'),
+                                   config('TWITTER_CONSUMER_SECRET'))
+TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'),
+                              config('TWITTER_ACCESS_TOKEN_SECRET'))
+TWITTER = tweepy.API(TWITTER_AUTH)
+
+BASILICA = basilica.Connection(config('BASILICA_KEY'))
+
+
+def add_or_update_user(username):
+    """Add or update a user and their tweets. Throw an error if not a Twitter user."""
+    try:
+        twitter_user = TWITTER.get_user(username)
+        db_user = (User.query.get(twitter_user.id) or
+                   User(id=twitter_user.id, name=twitter_user.screen_name))
+        DB.session.add(db_user)
+        tweets = twitter_user.timeline(
+            count=200, exclude_replies=True, include_rts=False,
+            tweet_mode='extended', since_id=db_user.newest_tweet_id)
+        if tweets:
+            db_user.newest_tweet_id = tweets[0].id
+        for tweet in tweets:
+            embedding = BASILICA.embed_sentence(tweet.full_text,
+                                                model='twitter')
+            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300],
+                            embedding=embedding)
+            db_user.tweets.append(db_tweet)
+            DB.session.add(db_tweet)
+    except Exception as e:
+        print('Error processing {}: {}'.format(username, e))
+        raise e
+    else:
+        DB.session.commit()
+        return db_user
